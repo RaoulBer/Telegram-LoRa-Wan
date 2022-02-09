1e991a064021536a1e44bb48627b9785146ba46a
Raoul B <raoulb97@gmail.com>
2022-02-09 17:09:46 +0100

    Telegram bot connection established

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,9 @@
   #include <ESP8266WiFi.h>
   #include <ESPAsyncTCP.h>
 #endif
-#include <ESPAsyncWebServer.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h> // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <ArduinoJson.h>
 
 // LoRa WAN module
 #define ss 5
@@ -16,107 +18,15 @@
 #define dio0 2
 
 // WIFI CONNECTION FOR HTTP SERVER AND CREDENTIALS
-const char* ssid = "scwl";
-const char* password = "*seitcomAccess2021!";
+const char* ssid = "iPhone von Paul";
+const char* password = "Paul1234";
 
-const char* http_username = "admin";
-const char* http_password = "admin";
+// Initialize Telegram BOT
+#define CHAT_ID "5110378746"
+#define BOTtoken "5160701458:AAFp1DJqnpTpcO7WYxB7bc4kQSlUOUP3WhI"  // your Bot Token (Get from Botfather)
 
-const char* PARAM_INPUT_1 = "state";
-
-const int output = 4;
-
-// HTTP WEB SERVER
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <title>ESP Web Server</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    h2 {font-size: 2.6rem;}
-    body {max-width: 600px; margin:0px auto; padding-bottom: 10px;}
-    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
-    .switch input {display: none}
-    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 34px}
-    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 68px}
-    input:checked+.slider {background-color: #2196F3}
-    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
-  </style>
-</head>
-<body>
-  <h2>ESP Web Server</h2>
-  <button onclick="logoutButton()">Logout</button>
-  <p>Ouput - GPIO 2 - State <span id="state">%STATE%</span></p>
-  %BUTTONPLACEHOLDER%
-<script>function toggleCheckbox(element) {
-  var xhr = new XMLHttpRequest();
-  if(element.checked){ 
-    xhr.open("GET", "/update?state=1", true); 
-    document.getElementById("state").innerHTML = "ON";  
-  }
-  else { 
-    xhr.open("GET", "/update?state=0", true); 
-    document.getElementById("state").innerHTML = "OFF";      
-  }
-  xhr.send();
-}
-function logoutButton() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/logout", true);
-  xhr.send();
-  setTimeout(function(){ window.open("/logged-out","_self"); }, 1000);
-}
-</script>
-</body>
-</html>
-)rawliteral";
-
-
-const char logout_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-  <p>Logged out or <a href="/">return to homepage</a>.</p>
-  <p><strong>Note:</strong> close all web browser tabs to complete the logout process.</p>
-</body>
-</html>
-)rawliteral";
-
-String outputState() {
-  if(digitalRead(output))
-  {
-    return "checked";
-  }
-
-  return "";
-}
-
-// Replaces placeholder with button section in your web page
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "BUTTONPLACEHOLDER"){
-    String buttons ="";
-    String outputStateValue = outputState();
-    buttons+= "<p><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"output\" " + outputStateValue + "><span class=\"slider\"></span></label></p>";
-    return buttons;
-  }
-  if (var == "STATE"){
-    if(digitalRead(output)){
-      return "ON";
-    }
-    else {
-      return "OFF";
-    }
-  }
-  return String();
-}
-
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
 
 void setup() {
   Serial.begin(9600);
@@ -124,7 +34,6 @@ void setup() {
 
   //Initialize LoRa WAN Receiver
   Serial.println("LoRa Receiver");
-  pinMode(output, OUTPUT);
   LoRa.setPins(ss, rst, dio0);
 
   if (!LoRa.begin(915E6)) {
@@ -133,7 +42,11 @@ void setup() {
   }
   
   // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+  #ifdef ESP32
+    client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  #endif
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
@@ -141,44 +54,6 @@ void setup() {
 
   // Print ESP Local IP Address
   Serial.println(WiFi.localIP());
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    if(!request->authenticate(http_username, http_password))
-      return request->requestAuthentication();
-    request->send_P(200, "text/html", index_html, processor);
-  });
-    
-  server.on("/logout", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(401);
-  });
-
-  server.on("/logged-out", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", logout_html, processor);
-  });
-
-  // Send a GET request to <ESP_IP>/update?state=<inputMessage>
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    if(!request->authenticate(http_username, http_password))
-      return request->requestAuthentication();
-    String inputMessage;
-    String inputParam;
-    // GET input1 value on <ESP_IP>/update?state=<inputMessage>
-    if (request->hasParam(PARAM_INPUT_1)) {
-      inputMessage = request->getParam(PARAM_INPUT_1)->value();
-      inputParam = PARAM_INPUT_1;
-      digitalWrite(output, inputMessage.toInt());
-    }
-    else {
-      inputMessage = "No message sent";
-      inputParam = "none";
-    }
-    Serial.println(inputMessage);
-    request->send(200, "text/plain", "OK");
-  });
-
-  // Start server
-  server.begin();
 }
 
 void loop() {
@@ -192,6 +67,8 @@ void loop() {
     while (LoRa.available()) {
       Serial.print((char)LoRa.read());
     }
+
+    bot.sendMessage(CHAT_ID, "Post ist da!");
 
     // print RSSI of packet
     Serial.print("' with RSSI ");
